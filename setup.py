ceb83a5c09534269394bb961ee674bd68bf8f967
mcnakhaee <mcnakhaee@gmail.org>
2020-08-07 16:42:13 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pydadegan',
+      version='0.0.1',
+      description='A package that contains several useful Persian datasets',
+      author='Muhammad Chenariyan Nakhaee',
+      author_emai='mcnakhaee@gmail.com',
+      packages = ['pydadegan'],
+      install_requires=['pandas', 'seaborn', 'numpy'])
